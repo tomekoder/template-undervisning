56a51a416a4864bf5ae19d48df650bebe9243222
tomekoder <tomekoder@gmail.com>
2024-12-02 13:46:16 +0100

    legg til og fjern virker

--- script.js
+++ script.js
@@ -3,6 +3,75 @@
 // Flat Array:
 const fruitBowl = ["Apple", "Pear", "Grapes"];
 
+fruitBowl.push("Banana");
+fruitBowl.push("Orange");
+fruitBowl.pop();
+
+fruitBowl.toString();
+
+console.log(fruitBowl.toString());
+
+//oppgave:
+const appleColors = [];
+
+appleColors.push("Green");
+appleColors.push("Yellow");
+appleColors.push("Red");
+
+appleColors.toString();
+
+console.log(appleColors.toString());
+
+//
+console.log("________________________________________________");
+//
+
+// hente inn elementer/fetch elements
+const inputField = document.querySelector("#inputField")
+const addFront = document.querySelector("#addFront")
+const removeFront = document.querySelector("#removeFront")
+const addBack = document.querySelector("#addBack")
+const removeBack = document.querySelector("#removeBack")
+const outputField = document.querySelector("#outputField")
+
+console.log(inputField, addFront, removeFront, addBack, removeBack, outputField)
+
+const outputArray = [];
+
+// gir elementer hendelser/give elements events
+
+addFront.addEventListener("click", function () {
+    console.log("button clicked!!");
+  outputArray.unshift(inputField.value);
+
+     console.log(outputArray);
+
+  outputField.textContent = outputArray
+})
+
+removeFront.addEventListener("click", function() {
+  console.log("buton click")
+  outputArray.pop(inputField.value);
+  console.log(outputArray);
+  outputField.textContent = outputArray
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Object with Arrays:
 const biggerFruitBowl = {
   fruit: ["Apple", "Pear", "Grapes"],
@@ -17,4 +86,5 @@ const evenBiggerFruitBowl = {
     "Grapes",
   ],
   berries: ["Banana", "Strawberry"],
-};
+}; 
+*/
