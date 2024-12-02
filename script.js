07e43b62385bedd2cf175e6d016fe6c5d8183805
tomekoder <tomekoder@gmail.com>
2024-12-02 10:02:39 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// General examples of the use of arrays:
+
+// Flat Array:
+const fruitBowl = ["Apple", "Pear", "Grapes"];
+
+// Object with Arrays:
+const biggerFruitBowl = {
+  fruit: ["Apple", "Pear", "Grapes"],
+  berries: ["Banana", "Strawberry"],
+};
+
+// Nested Arrays in Objects
+const evenBiggerFruitBowl = {
+  fruit: [
+    { apple: ["Red apple", "Green Apple", "Yellow Apples"] },
+    "Pear",
+    "Grapes",
+  ],
+  berries: ["Banana", "Strawberry"],
+};
